fd59d11c46a567ad06012fc377a446ba19776019
anil <anilbhomi201@gmail.com>
2018-05-13 21:24:35 +0545

    initial commit

---
+++ app/src/main/java/com/aaneal/databindinglivedata/viewModel/LoginViewModel.java
@@ -0,0 +1,34 @@
+package com.aaneal.databindinglivedata.viewModel;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
+
+import com.aaneal.databindinglivedata.model.LoginModel;
+import com.aaneal.databindinglivedata.remote.LoginRepository;
+
+public class LoginViewModel extends ViewModel {
+
+    public ObservableField<String> email = new ObservableField<>("");
+    public ObservableField<String> password = new ObservableField<>("");
+
+    public MutableLiveData<LoginModel> response;
+
+    private LoginRepository loginRepository;
+
+    public LoginViewModel() {
+        loginRepository = new LoginRepository();
+        email.set("anil");
+        password.set("bhomi");
+        response = loginRepository.getData(email.get(),password.get());
+
+    }
+
+    public void login(){
+        response = loginRepository.getData(email.get(),password.get());
+    }
+
+    public MutableLiveData<LoginModel> getResponse() {
+        return response;
+    }
+}
---
+++ app/src/main/java/com/aaneal/databindinglivedata/viewModel/NameModel.java
@@ -0,0 +1,36 @@
+package com.aaneal.databindinglivedata.viewModel;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
+import android.util.Log;
+
+import com.aaneal.databindinglivedata.remote.UserRepository;
+
+public class NameModel extends ViewModel {
+
+    public MutableLiveData<String> data;
+
+    private UserRepository userRepository;
+
+    public ObservableField<String> nameVal = new ObservableField<>("");
+
+    public NameModel(){
+        userRepository = new UserRepository();
+        userRepository.loginUser("anil");
+        data = userRepository.getResponse();
+    }
+
+    public void login(){
+        userRepository.loginUser("anil1");
+        data = userRepository.getResponse();
+
+        Log.i("debug",data.getValue());
+    }
+
+    public MutableLiveData<String> getData() {
+
+        return data;
+
+    }
+}
